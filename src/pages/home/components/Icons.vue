d76099a7b8e378da4bd90a8ea193a877ace4ff4e
1393925530 <zzc1393925530@gmail.com>
2019-05-14 20:41:51 +0800

    transfer project from gitee

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="icons">
-    <swiper :options="swiperOption">
+    <swiper>
       <swiper-slide v-for="(page, index) of pages"
                     :key="index">
         <div class="icon"
@@ -25,7 +25,7 @@ export default {
       iconList: [{
         id: '0001',
         imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
-        desc: '景点门票哈哈哈哈哈哈哈哈哈'
+        desc: '景点门票'
       }, {
         id: '0002',
         imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
@@ -83,33 +83,35 @@ export default {
 .icons >>> .swiper-container
   height 0
   padding-bottom 50%
-.icon
-  position relative
-  overflow hidden
-  float left
-  width 25%
-  height 0
-  padding-bottom 25%
-  .icon-img
-    position absolute
-    top 0
-    left 0
-    right 0
-    bottom 0.44rem
-    box-sizing border-box
-    padding 0.1rem
-    .icon-img-content
-      display block
-      margin 0 auto
-      height 100%
-  .icon-desc
-    position absolute
-    left 0
-    right 0
-    bottom 0
-    height 0.44rem
-    line-height 0.44rem
-    text-align center
-    color $darkTextColor
-    ellipsis()
+.icons
+  margin-top 0.1rem
+  .icon
+    position relative
+    overflow hidden
+    float left
+    width 25%
+    height 0
+    padding-bottom 25%
+    .icon-img
+      position absolute
+      top 0
+      left 0
+      right 0
+      bottom 0.44rem
+      box-sizing border-box
+      padding 0.1rem
+      .icon-img-content
+        display block
+        margin 0 auto
+        height 100%
+    .icon-desc
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      height 0.44rem
+      line-height 0.44rem
+      text-align center
+      color $darkTextColor
+      ellipsis()
 </style>
